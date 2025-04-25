99961d0009db69d55c20e2d25213dd3b8d61e6d9
Basile <jf-b@outlook.be>
2025-04-25 15:48:20 +0200

    Update litert.cpp

--- components/litert/litert.cpp
+++ components/litert/litert.cpp
@@ -1,5 +1,21 @@
 #include "litert.h"
 #include "esphome/core/log.h"
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+
+// Globals, used for compatibility with Arduino-style sketches.
+namespace {
+const tflite::Model* model = nullptr;
+tflite::MicroInterpreter* interpreter = nullptr;
+TfLiteTensor* input = nullptr;
+TfLiteTensor* output = nullptr;
+int inference_count = 0;
+
+constexpr int kTensorArenaSize = 2000;
+uint8_t tensor_arena[kTensorArenaSize];
+}  // namespace
 
 namespace esphome {
 namespace litert {
