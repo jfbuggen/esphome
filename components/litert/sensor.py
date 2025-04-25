d70ad7c1f11ea387f494aeebd4d12d892b7b0ff2
Basile <jf-b@outlook.be>
2025-04-25 14:34:54 +0200

    Update sensor.py

--- components/litert/sensor.py
+++ components/litert/sensor.py
@@ -136,5 +136,5 @@ async def to_code(config):
     rhs = [int(x) for x in content]
     # Create an array which will reside in program memory and configure the sensor instance to use it
     model_arr = cg.progmem_array(config[CONF_RAW_DATA_ID], rhs)
-    cg.add(var.set_model(model_arr, len(rhs))
+    cg.add(var.set_model(model_arr, len(rhs)))
 
