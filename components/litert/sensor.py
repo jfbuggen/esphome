6ea0ec9d9b38e82f13cc01715edeeca1951fc60f
Basile <jf-b@outlook.be>
2025-04-25 18:29:56 +0200

    Update sensor.py

--- components/litert/sensor.py
+++ components/litert/sensor.py
@@ -150,7 +150,7 @@ async def to_code(config):
     )
     # Need to point explicitly to the flatbuffer header files
     cg.add_build_flag("-v")
-    cg.add_build_flag('-I"../../../flatbuffers/include"')
+    cg.add_build_flag('-I"${platformio.workspace_dir}/${env}/flatbuffers/include"')
     cg.add_build_flag('-I"${platformio.libdeps_dir}/third_party/flatbuffers/include"')
     cg.add_build_flag("-I.piolibdeps/test-litert/TensorFlow/third_party/gemmlowp")
     # Set deeper ldf mode to ensure the compiler finds the right header files accross component code and library code
