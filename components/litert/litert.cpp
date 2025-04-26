01db417f69ddbb8b9d975bc8101068d5cd2e6788
Basile <jf-b@outlook.be>
2025-04-26 17:22:00 +0200

    Update litert.cpp

--- components/litert/litert.cpp
+++ components/litert/litert.cpp
@@ -7,12 +7,6 @@
 
 // Globals, used for compatibility with Arduino-style sketches.
 namespace {
-const tflite::Model* model = nullptr;
-tflite::MicroInterpreter* interpreter = nullptr;
-TfLiteTensor* input = nullptr;
-TfLiteTensor* output = nullptr;
-int inference_count = 0;
-
 constexpr int kTensorArenaSize = 2000;
 uint8_t tensor_arena[kTensorArenaSize];
 }  // namespace
@@ -24,7 +18,51 @@ static const char *const TAG = "litert";
 
 void LiteRTComponent::setup() {
 
-  ESP_LOGV(TAG, "Setup()");  
+  ESP_LOGV(TAG, "Setup()");
+  
+  // Map the model into a usable data structure. This doesn't involve any
+  // copying or parsing, it's a very lightweight operation.
+  if (this->model_data_ == nullptr) {
+    ESP_LOGE(TAG, "No model provided!");
+    this->mark_failed();
+    return;
+  }
+  
+  this->model_ = tflite::GetModel(this->model_data_);
+  if (this->model_->version() != TFLITE_SCHEMA_VERSION) {
+    ESP_LOGE(TAG, "Model provided is schema version %d not equal to supported "
+                "version %d.", this->model_->version(), TFLITE_SCHEMA_VERSION);
+    this->mark_failed();
+    return;
+  }
+
+  // Pull in only the operation implementations we need.
+  static tflite::MicroMutableOpResolver<1> resolver;
+  if (resolver.AddFullyConnected() != kTfLiteOk) {
+    ESP_LOGE(TAG, "Could not set-up resolver");
+    this->mark_failed();
+    return;
+  }
+
+  // Build an interpreter to run the model with.
+  static tflite::MicroInterpreter static_interpreter(
+      this->model_, resolver, tensor_arena, kTensorArenaSize);
+  this->interpreter_ = &static_interpreter;
+
+  // Allocate memory from the tensor_arena for the model's tensors.
+  TfLiteStatus allocate_status = this->interpreter_->AllocateTensors();
+  if (allocate_status != kTfLiteOk) {
+    ESP_LOGE(TAG, "AllocateTensors() failed");
+    this->mark_failed();
+    return;
+  }
+
+  // Obtain pointers to the model's input and output tensors.
+  this->input_ = interpreter->input(0);
+  this->output_ = interpreter->output(0);
+
+  // Keep track of how many inferences we have performed.
+  this->inference_count_ = 0;
 }
 
 void LiteRTComponent::loop() {
