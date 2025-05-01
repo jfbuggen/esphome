a5a060303ac20a9d56998fdb7d2aec59a440eed6
Basile <jf-b@outlook.be>
2025-05-01 18:32:06 +0200

    Update __init__.py

--- components/litert/__init__.py
+++ components/litert/__init__.py
@@ -103,7 +103,7 @@ CONFIG_SCHEMA = (
             cv.GenerateID(): cv.declare_id(LiteRTComponent),
             cv.Required(CONF_FILE): cv.Any(validate_file_shorthand, TYPED_FILE_SCHEMA),
             cv.GenerateID(CONF_RAW_DATA_ID): cv.declare_id(cg.uint8),
-            cv.GenerateID(CONF_OP_ID): cv.declare_id(MicroMutableOpResolver),
+            cv.GenerateID(CONF_OP_ID): cv.declare_id(MicroOpResolver),
         }
     )
 )
@@ -133,8 +133,8 @@ async def to_code(config):
 
     opcount = CONF_OP_COUNT
     resolver = MicroMutableOpResolver.template(opcount)
-    #rhs = resolver.new()
-    rhs = cg.Pvariable(config[CONF_OP_ID], MicroMutableOpResolver.template(opcount), MicroOpResolver)
+    res = resolver.new()
+    rhs = cg.Pvariable(config[CONF_OP_ID], res, MicroOpResolver)
     #cg.add(var.set_op_resolver(res))
 
     var = cg.new_Pvariable(config[CONF_ID], rhs)
