20c92a903d7de67436583c00307721b4de2ca485
Basile <jf-b@outlook.be>
2025-05-01 14:26:48 +0200

    Update litert.h

--- components/litert/litert.h
+++ components/litert/litert.h
@@ -8,7 +8,7 @@ namespace litert {
 
 class LiteRTComponent : public Component {
  public:
-  LiteRTComponent(tflite::MicroOpResolver& op_resolver) { this->op_resolver_ = op_resolver; }
+  LiteRTComponent(tflite::MicroOpResolver& op_resolver) : op_resolver_(op_resolver) { }
   void setup() override;
   void loop() override;
   void dump_config() override;
@@ -17,7 +17,7 @@ class LiteRTComponent : public Component {
     this->model_data_ = model_data;
     this->model_data_length_ = len;
   }
-  void set_op_resolver (tflite::MicroOpResolver& op_resolver) { this->op_resolver_ = op_resolver; }
+#  void set_op_resolver (tflite::MicroOpResolver& op_resolver) { this->op_resolver_ = op_resolver; }
 
  protected:
   uint8_t const *model_data_{nullptr};
